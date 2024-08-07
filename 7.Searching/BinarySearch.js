6f9f0e4fbca6a7a58d0ce843515a95c8222f6026
Yash <yashdesh095@gmail.com>
2024-08-07 18:43:31 +0530

    Completed Binary Search

---
+++ 7.Searching/BinarySearch.js
@@ -0,0 +1,43 @@
+/*
+Binary Search Exercise
+Write a function called binarySearch which accepts a sorted array and a value and returns the index at which the value exists. Otherwise, return -1.
+
+Examples:
+
+binarySearch([1,2,3,4,5],2) // 1
+binarySearch([1,2,3,4,5],3) // 2
+binarySearch([1,2,3,4,5],5) // 4
+binarySearch([1,2,3,4,5],6) // -1
+binarySearch([
+  5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 
+  40, 44, 64, 79, 84, 86, 95, 96, 98, 99
+], 10) // 2
+binarySearch([
+  5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 
+  40, 44, 64, 79, 84, 86, 95, 96, 98, 99
+], 95) // 16
+binarySearch([
+  5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 
+  40, 44, 64, 79, 84, 86, 95, 96, 98, 99
+], 100) // -1
+*/
+
+function binarySearch (arr,num){
+let left = 0
+let right = arr.length - 1
+let middle
+while(left <= right){
+     middle = Math.floor((right + left)/2)
+    if(arr[middle] == num) return middle;
+    if(arr[middle] > num){
+        right = middle - 1
+    }else{
+        left = middle + 1 
+    }
+}
+    return -1
+}
+console.log(binarySearch([
+    5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 
+    40, 44, 64, 79, 84, 86, 95, 96, 98, 99
+  ], 95) );
